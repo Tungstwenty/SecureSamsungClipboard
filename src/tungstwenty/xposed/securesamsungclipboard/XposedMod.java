14bc75b192152b0e51493371025a3643eb2bfaa4
Tungstwenty <tungstwenty@gmail.com>
2013-03-26 17:26:03 +0000

    Only clear clipboard history when empty text is copied

--- src/tungstwenty/xposed/securesamsungclipboard/XposedMod.java
+++ src/tungstwenty/xposed/securesamsungclipboard/XposedMod.java
@@ -3,6 +3,7 @@ package tungstwenty.xposed.securesamsungclipboard;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.findMethodExact;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 import java.lang.reflect.Method;
 
@@ -14,6 +15,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class XposedMod implements IXposedHookLoadPackage {
 
 	private static final String PACKAGE_ANDROID = "android";
+	private static final String CLASS_CLIPBOARDDATATEXT = "android.sec.clipboard.data.list.ClipboardDataText";
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
@@ -28,13 +30,22 @@ public class XposedMod implements IXposedHookLoadPackage {
 				    new XC_MethodHook() {
 					    @Override
 					    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+						    // Check if adding empty string, which triggers clearing the clipboard
+
+						    if (!param.args[0].getClass().getName().equals(CLASS_CLIPBOARDDATATEXT))
+							    return; // Not adding a text clip
+
+						    String value = (String) getObjectField(param.args[0], "mValue");
+						    if (value != null && value.length() > 0)
+							    return; // Not adding an empty string
+
 						    // Erase all entries in the clipboard history
 						    int i = (Integer) mthSize.invoke(param.thisObject);
 						    while (i-- > 0)
 							    mthRemoveData.invoke(param.thisObject, 0);
+						    param.setResult(true);
 					    }
 				    });
-
 			} catch (Throwable t) {
 				XposedBridge.log(t);
 			}
