80dad2a20bfdf7336a67f54df3f8f444071ee3ba
Tungstwenty <tungstwenty@gmail.com>
2013-03-27 00:49:56 +0000

    Display Toast when clipboard history is cleared

--- src/tungstwenty/xposed/securesamsungclipboard/XposedMod.java
+++ src/tungstwenty/xposed/securesamsungclipboard/XposedMod.java
@@ -1,26 +1,69 @@
 package tungstwenty.xposed.securesamsungclipboard;
 
+import static de.robv.android.xposed.XposedBridge.hookMethod;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.findConstructorExact;
 import static de.robv.android.xposed.XposedHelpers.findMethodExact;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 
 import java.lang.reflect.Method;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.XModuleResources;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class XposedMod implements IXposedHookLoadPackage {
+public class XposedMod implements IXposedHookZygoteInit, IXposedHookLoadPackage, IXposedHookInitPackageResources {
 
 	private static final String PACKAGE_ANDROID = "android";
 	private static final String CLASS_CLIPBOARDDATATEXT = "android.sec.clipboard.data.list.ClipboardDataText";
+
+	private static Handler mHandler = null;
+
+	private String modulePath;
+	private String msgClipboardCleared = null;;
+
+	@Override
+	public void initZygote(StartupParam startupParam) throws Throwable {
+		modulePath = startupParam.modulePath;
+	}
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 
 		if (lpparam.packageName.equals(PACKAGE_ANDROID)) {
+			try {
+				hookMethod(
+				    findConstructorExact(findClass("com.android.server.sec.InternalClipboardExService", null),
+				        Context.class), new XC_MethodHook() {
+					    @SuppressLint("HandlerLeak")
+					    @Override
+					    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+						    if (mHandler == null && param.args[0] instanceof Context) {
+							    final Context ctx = (Context) param.args[0];
+							    mHandler = new Handler() {
+								    public void handleMessage(Message msg) {
+									    if (msg.arg1 == 1)
+										    Toast.makeText(ctx, msgClipboardCleared, Toast.LENGTH_SHORT).show();
+								    }
+							    };
+						    }
+					    }
+				    });
+			} catch (Throwable t) {
+				XposedBridge.log(t);
+			}
 			try {
 				final Class<?> clsClipDataMgr = findClass("android.sec.clipboard.data.ClipboardDataMgr",
 				    lpparam.classLoader);
@@ -43,6 +86,13 @@ public class XposedMod implements IXposedHookLoadPackage {
 						    int i = (Integer) mthSize.invoke(param.thisObject);
 						    while (i-- > 0)
 							    mthRemoveData.invoke(param.thisObject, 0);
+
+						    // Notify the user that the history was cleared
+						    if (mHandler != null && msgClipboardCleared != null) {
+							    Message msg = mHandler.obtainMessage();
+							    msg.arg1 = 1;
+							    mHandler.sendMessageDelayed(msg, 200L);
+						    }
 						    param.setResult(true);
 					    }
 				    });
@@ -51,4 +101,14 @@ public class XposedMod implements IXposedHookLoadPackage {
 			}
 		}
 	}
+
+	@Override
+	public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
+		if (msgClipboardCleared == null && resparam.packageName.equals(PACKAGE_ANDROID)) {
+			// Reference the resources from this mod
+			XModuleResources modRes = XModuleResources.createInstance(modulePath, resparam.res);
+			msgClipboardCleared = modRes.getString(R.string.msg_clipboard_cleared);
+		}
+	}
+
 }
